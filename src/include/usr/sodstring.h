5c8d7a6179bb8247c33388292b7f8fccf3ac3709
devenuta <devenuta@ae067edc-89e1-11dd-887c-ed04d4280016>
2008-10-04 14:08:46 +0000

    Agregada sodstring

---
+++ src/include/usr/sodstring.h
@@ -0,0 +1,14 @@
+/**
+\file sodstring.h
+\brief Funciones de manejo de cadenas de caracteres
+ */
+#ifndef __SODSTRING_H_
+#define __SODSTRING_H_
+
+inline unsigned char* memcpy(unsigned char *ucpDestino, 
+				  unsigned char *ucpOrigen, 
+				  unsigned int uiTamanio);
+
+
+#endif
+
---
+++ src/usr/sodstring.c
@@ -0,0 +1,42 @@
+#include <usr/sodstring.h>
+
+/* ATENCION: Este archivo esta basado en kernel/libk/string.c
+ * Cualquier modificacion que se haga en este archivo debe ser tenida en cuenta
+ * para ser incluida en el archivo original mencionado y viceversa.
+ *
+ * Puede que muchas de las funciones y partes del codigo de hallen comentadas.
+ * Esto no quiere decir que no funcionen o sean inapropiadas, es solo que al
+ * momento de generar este archivo no se tuvo el tiempo necesario para probarlas
+ */
+
+
+/**
+ * @brief Copia 'size' bytes de 'orig' a 'dest'
+ * @date 
+ */
+inline unsigned char* memcpy(
+        unsigned char *ucpDestino,
+        unsigned char *ucpOrigen,
+        unsigned int uiTamanio )
+{
+    /* Esta version en assembler es de 3 a 12 veces mas rapida (depende de la
+     * cantidad de bytes a copiar) que la version anterior hecha en C.
+     */
+    int d0, d1, d2;
+
+    __asm__ __volatile__(
+        "rep ; movsl\n\t"
+        "testb $2,%b4\n\t"
+        "je 1f\n\t"
+        "movsw\n"
+        "1:\ttestb $1,%b4\n\t"
+        "je 2f\n\t"
+        "movsb\n"
+        "2:"
+        : "=&c" (d0), "=&D" (d1), "=&S" (d2)
+        :"0" (uiTamanio/4), "q" (uiTamanio),
+            "1" ((long) ucpDestino),"2" ((long) ucpOrigen)
+        : "memory");
+
+    return ucpDestino;
+}
