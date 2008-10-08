d257ba288644be3ad89fe99d710688a72838483a
pablosambuco <pablosambuco@ae067edc-89e1-11dd-887c-ed04d4280016>
2008-10-08 14:29:28 +0000

    Cambios en la instalacion de bochs y en el clean de los Makefiles. Ahora no queda nada extra para los commits

--- instalar_bochs.sh
+++ instalar_bochs.sh
@@ -1,35 +1,40 @@
 #!/bin/sh
+LOG=salida.log
+ERROR=errores.log
+
 echo -n "Logueandose como root..."
-sudo echo ""
+sudo echo "" 1>${LOG} 2>${ERROR}
 
 #echo "Eliminando otras versiones"
-#sudo apt-get remove --force-yes bochs 1>/dev/null 2>/dev/null
+sudo apt-get remove --force-yes bochs 1>>${LOG} 2>>${ERROR}
 
 echo "Verificando paquetes necesarios para compilar"
-sudo apt-get install --force-yes byacc 1>/dev/null 2>/dev/null 
+sudo apt-get install --force-yes byacc libwxgtk2.6-dev wx2.6-headers wx-common libxmu-dev libxmuu-dev 1>>${LOG} 2>>${ERROR} 
+
 # Bochs, baja el codigo fuente, compila e instala
 echo -n "Bajando "
-wget http://ufpr.dl.sourceforge.net/sourceforge/bochs/bochs-2.3.7.tar.gz 1>/dev/null 2>/dev/null
+wget http://ufpr.dl.sourceforge.net/sourceforge/bochs/bochs-2.3.7.tar.gz 1>>${LOG} 2>>${ERROR} 
 if test $? -eq 0
 then
    echo "OK"
    echo -n "Descomprimiendo "   
-   tar -xvvzf bochs-2.3.7.tar.gz 1>/dev/null 2>/dev/null
+   tar -xvvzf bochs-2.3.7.tar.gz  1>>${LOG} 2>>${ERROR}
    if test $? -eq 0 
    then
       echo "OK"
-    #  yes | rm bochs-2.3.7.tar.gz
+      yes | rm bochs-2.3.7.tar.gz
       cd bochs-2.3.7
       echo -n "Configurando "
-      sudo sh ./configure --with-x --enable-show-ips --enable-debugger --enable-readline --enable-disasm --enable-idle-hack 1>/dev/null 2>/dev/null
+      sudo sh ./configure --with-x --enable-show-ips --enable-debugger --enable-readline --enable-disasm --enable-idle-hack 1>>${LOG} 2>>${ERROR} 
       if test $? -eq 0 
       then
          echo  "OK"
          echo -n "Instalando "
-         sudo make install 1>/dev/null 2>/dev/null
+         sudo make install 1>>${LOG} 2>>${ERROR} 
          if test $? -eq 0 
          then
             echo "OK"
+            rm ${LOG} ${ERROR}
          else 
             echo "ERROR"
          fi
@@ -45,10 +50,11 @@ fi
 cd ..
 yes | rm -r bochs-2.3.7 
 echo -n "Instalando paquetes extra "
-sudo apt-get install --force-yes vgabios 1>/dev/null 2>/dev/null
+sudo apt-get install --force-yes vgabios 1>>${LOG} 2>>${ERROR} 
 if test $? -eq 0
 then
    echo "OK"
+   rm ${LOG} ${ERROR}
 else
    echo "ERROR"
 fi
