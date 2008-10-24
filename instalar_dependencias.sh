3a70f62f67dbe4a403a6eb6c7d72f349c99734c9
pablosambuco <pablosambuco@ae067edc-89e1-11dd-887c-ed04d4280016>
2008-10-24 03:31:32 +0000

    Cambiado el instalar_dependencias.sh... ya no me nombra más

--- instalar_dependencias.sh
+++ instalar_dependencias.sh
@@ -2,10 +2,10 @@
 apt-get update
 
 # Compilacion
-apt-get install --force-yes nasm exuberant-ctags build-essential glibc-doc manpages-dev
+apt-get install --force-yes nasm exuberant-ctags build-essential glibc-doc manpages-dev console-keymaps
 
 # Maquinas virtuales
-# Para tener todas las funcionalidades, convendria compilarlas a mano
+# Para tener todas las funcionalidades de bochs, usar instalar_bochs.sh
 apt-get install --force-yes qemu bximage gdb
 
 # Documentacion
@@ -13,15 +13,3 @@ apt-get install --force-yes doxygen graphviz
 
 # Subversion
 apt-get install --force-yes subversion
-
-# Para empezar a trabajar con SVN, ejecutar:
-#
-#   svn checkout svn://pablosambuco.no-ip.org:3690 Sodium --username APELLIDO
-#
-# eso crea un directorio Sodium en el directorio actual con la copia del
-# repositorio.
-# ATENCION: Puede tardar mucho tiempo! Mandarle un mail a Pablo Sambuco para
-# que apague el eMule :P
-#
-# Un buen GUI para SVN en Ubuntu es esvn
-#   sudo apt-get install esvn 
